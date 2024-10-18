a14890ee9a9c7fcd8c6335d6dd35d72cf3c16a7b
Sahil Chabra <sahilchabra1509@gmail.com>
2024-10-19 01:50:49 +0530

    Learned Controllers&routes and also deleted .env

--- src/app.js
+++ src/app.js
@@ -9,10 +9,16 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes
+//importing
+import userRouter from './routes/user.route.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app }
